80945551faa3ff248f47a654ae218827a89c5a92
Stars of the Future <162513898+q2dhsjj@users.noreply.github.com>
2026-09-10 22:39:36 +0800

    在html更改了一下错别字，在js增加了一段话

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
  function changeText() {
-        document.getElementById('msg').textContent = '你刚刚触发了一段 JavaScript。';
+        document.getElementById('msg').textContent = '你刚刚触发了一段 JavaScript(什么意思我也不知道)。';
 }
